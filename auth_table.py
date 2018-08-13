7ed43274f13e5bc383da9394fb9c178b876f1009
Tania Omelinska <omelinska@gmail.com>
2018-08-13 13:59:06 +0300

    adding password based authentification no hashing yet

---
+++ auth_table.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password = Column(String(32))
+
+
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from config_reader import ConfigReader
 from data_transformer import DataTransformer
-from json import loads
-from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from movie_tables import Movie, Country, Genre, movies_genres_association
+from json import loads
+from flask import Flask, jsonify, request
+from flask_httpauth import HTTPBasicAuth
+from auth_table import User
 
 
 app = Flask('MoviesREST')
+auth = HTTPBasicAuth()
 cr = ConfigReader()
 host, port, dbname, user, password = cr.get_database_config()
 engine = create_engine('postgresql://{0}:{1}@{2}:{3}/{4}'.format(user, password, host, port, dbname))
@@ -17,23 +20,44 @@ session = Session()
 db = DataTransformer()
 
 
+@auth.verify_password
+def verify_password(username, password):
+    stored_password = session.query(User.password).filter_by(username=username).first()[0]
+    if stored_password == password:
+        return True
+    else:
+        return False
+
+
 @app.route('/movies', methods=['GET'])
+@auth.login_required
 def get_movie():
-    result = session.query(Movie.id, Movie.title, Movie.year, Country.name, Genre.name).join(Country)\
-        .join(movies_genres_association).join(Genre).all()
+    result = session.query(Movie.id, Movie.title, Movie.year, Country.name, Genre.name) \
+        .join(Country, isouter=True) \
+        .join(movies_genres_association, isouter=True) \
+        .join(Genre, isouter=True) \
+        .all()
     return db.transform_dataset_into_json(result)
 
 
 @app.route('/movies/<id>', methods=['GET'])
+@auth.login_required
 def get_movies(id):
-    result = session.query(Movie.id, Movie.title, Movie.year, Country.name, Genre.name).join(Country)\
-        .join(movies_genres_association).join(Genre).filter(Movie.id == id).all()
+    result = session.query(Movie.id, Movie.title, Movie.year, Country.name, Genre.name) \
+        .join(Country, isouter=True) \
+        .join(movies_genres_association, isouter=True) \
+        .join(Genre, isouter=True) \
+        .filter(Movie.id == id)\
+        .all()
     return db.transform_row_into_json(result)
 
 
 @app.route('/movies/<id>', methods=['DELETE'])
+@auth.login_required
 def delete_movie(id):
-    session.query(Movie).filter(Movie.id == id).delete()
+    session.query(Movie)\
+        .filter(Movie.id == id)\
+        .delete()
     session.commit()
     message = 'Movie {} was deleted!'.format(id)
     dic = {'message': message}
@@ -41,14 +65,18 @@ def delete_movie(id):
 
 
 @app.route('/movies', methods=['POST'])
+@auth.login_required
 def add_movie():
     data = loads(request.data)
     title = data['title']
     year = data['year']
     country = data['country']
     genre = data['genre']
-    movie_row = Movie(title=title, year=year, country=session.query(Country).filter(Country.name == country).first())
-    movie_row.genre.append(session.query(Genre).filter(Genre.name == genre).first())
+    movie_row = Movie(title=title, year=year, country=session.query(Country)
+                      .filter(Country.name == country)
+                      .first())
+    movie_row.genre.append(session.query(Genre).filter(Genre.name == genre)
+                           .first())
     session.add(movie_row)
     session.commit()
     message = 'Movie {} was added!'.format(title)
@@ -57,19 +85,24 @@ def add_movie():
 
 
 @app.route('/movies/<id>', methods=['PUT'])
+@auth.login_required
 def update_movie(id):
     data = loads(request.data)
     title = data['title']
     year = data['year']
     country = data['country']
-    movie_row = session.query(Movie).filter_by(id=id).first()
+    movie_row = session.query(Movie)\
+        .filter_by(id=id)\
+        .first()
     dic = {}
     if movie_row is None:
         dic['message'] = 'Movie Not Found'
     else:
         movie_row.title = title
         movie_row.year = year
-        movie_row.country = session.query(Country).filter(Country.name == country).first()
+        movie_row.country = session.query(Country)\
+            .filter(Country.name == country)\
+            .first()
         session.commit()
         dic['message'] = 'Movie Was Updated'
     return jsonify(dic)
